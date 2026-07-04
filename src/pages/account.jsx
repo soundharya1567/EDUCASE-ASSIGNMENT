1c7dfd78621a5e5e6b302af51219b123dd3660ca
soundharya1567 <152592010+soundharya1567@users.noreply.github.com>
2026-07-05 02:17:15 +0530

    account.jsx

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -1,34 +1,27 @@
 import { useEffect, useState } from "react";
 import profileImage from "../assets/image1.avif";
-
 function Account() {
   const [user, setUser] = useState({});
-
   useEffect(() => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
-
     if (storedUser) {
       setUser(storedUser);
     }
   }, []);
-
   return (
     <div className="container">
       <div className="mobile">
         <div className="account">
           <h2>Account Settings</h2>
-
           <div className="profile-card">
             <div className="profile-image">
               <img src={profileImage} alt="Profile" />
             </div>
-
             <div className="profile-info">
               <h3>{user.name || "Marry Doe"}</h3>
               <p>{user.email || "Marry@Gmail.com"}</p>
             </div>
           </div>
-
           <p className="description">
             Lorem Ipsum Dolor Sit Amet, Consetetur Sadipscing Elitr, Sed Diam
             Nonumy Eirmod Tempor Invidunt Ut Labore Et Dolore Magna Aliquyam
